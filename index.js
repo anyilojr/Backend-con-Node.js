2452dcfabbd962e1ab98854002023e5a29b1058d
Anyilo Rangel <anyilojoserangelarias@gmail.com>
2023-02-10 17:12:04 -0500

    Final

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 // Importar middleware
 const { logErrors, errorHandler, boomErrorHandler } = require('./Middleware/error.handler'); //importar las funciones que se uilizarán
@@ -7,6 +8,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)){
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors());
 
 app.get('/', (req, res)=> {
   res.send('Hola mi server en express');
